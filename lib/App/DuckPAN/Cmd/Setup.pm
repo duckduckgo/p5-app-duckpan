a847c68b4d2871675f8326ae6148172d858fdc14
Torsten Raudssus <github@raudssus.de>
2012-02-12 01:51:51 +0100

    Using Dist::Zilla config if exist

--- lib/App/DuckPAN/Cmd/Setup.pm
+++ lib/App/DuckPAN/Cmd/Setup.pm
@@ -6,12 +6,15 @@ with qw( App::DuckPAN::Cmd );
 use MooX::Options;
 use Term::UI;
 use Term::ReadLine;
+use Email::Valid;
 
 option override => ( is => 'ro' );
 
 option user => (
 	is => 'rw',
 	lazy => 1,
+	predicate => 'has_user',
+	clearer => 'clear_user',
 	default => sub { shift->get_user }
 );
 
@@ -20,13 +23,30 @@ sub get_user { shift->term->get_reply( prompt => 'What is your username on https
 option pass => (
 	is => 'rw',
 	lazy => 1,
+	predicate => 'has_pass',
 	default => sub { shift->get_pass }
 );
 
-sub get_pass {
-	my $self = shift;
-	$self->term->get_reply( prompt => 'What is your password on https://dukgo.com/ ? ' );
-}
+sub get_pass { shift->term->get_reply( prompt => 'What is your password on https://dukgo.com/ ? ' ) }
+
+option name => (
+	is => 'rw',
+	lazy => 1,
+	predicate => 'has_name',
+	default => sub { shift->get_name }
+);
+
+sub get_name { shift->term->get_reply( prompt => 'What is your name (real name not required) ? ' ) }
+
+option email => (
+	is => 'rw',
+	isa => sub { Email::Valid->address(shift); },
+	lazy => 1,
+	predicate => 'has_email',
+	default => sub { shift->get_email }
+);
+
+sub get_email { shift->term->get_reply( prompt => 'What is your email (public in your release) ? ' ) }
 
 has term => (
 	is => 'ro',
@@ -39,22 +59,91 @@ sub _build_term { Term::ReadLine->new('duckpan') }
 sub run {
 	my ( $self ) = @_;
 	$self->app->check_requirements;
-	print "\nGetting your https://dukgo.com/ user informations\n\n";
-	$self->setup_dukgo;
+	if (my $dzil_config = $self->app->perl->get_dzil_config) {
+		print "\nFound existing Dist::Zilla config!\n\n";
+		my $name = $dzil_config->{'%User'}->{name};
+		my $email = $dzil_config->{'%User'}->{email};
+		my $user = $dzil_config->{'%DUKGO'}->{username};
+		my $pass = $dzil_config->{'%DUKGO'}->{password};
+		print "Name: ".$name."\n" if $name;
+		print "Email: ".$email."\n" if $email;
+		print "Username at https://dukgo.com/: ".$user."\n" if $user;
+		print "Password at https://dukgo.com/: ".$pass."\n" if $pass;
+		if ($name || $email || $user || $pass) {
+			print "\n";
+			if ($self->term->ask_yn( prompt => 'Do you wanna use those? ' )) {
+				if ($user && $pass) {
+					print "\nChecking your account on https://dukgo.com/... ";
+					if ($self->app->checking_dukgo_user($user,$pass)) {
+						print "success!\n";
+						$self->user($user);
+						$self->pass($pass);
+					} else {
+						print "failed!\n";
+					}
+				}
+				$self->name($name) if $name;
+				$self->email($email) if $email;
+			}
+		}
+	}
+	unless ($self->has_name && $self->has_email) {
+		print "\nWe require some general information about you\n\n";
+		$self->setup_name unless $self->has_name;
+		$self->setup_email unless $self->has_email;
+	}
+	unless ($self->has_user && $self->has_pass) {
+		print "\nGetting your https://dukgo.com/ user informations\n\n";
+		$self->setup_dukgo;
+	}
 	print "\nInitalizing DuckPAN Environment\n\n";
 	$self->setup_configdir;
 	print "\nInitalizing Dist::Zilla for Perl5\n";
 	$self->app->perl->setup(
 		dukgo_user => $self->user,
 		dukgo_pass => $self->pass,
+		name => $self->name,
+		email => $self->email,
 	);
+}
+
+sub setup_name {
+	my ( $self ) = @_;
+	my $name = $self->get_name;
+	if ($name) {
+		$self->name($name);
+	} else {
+		print "We need some kind of name!\n";
+		if ($self->term->ask_yn( prompt => 'Wanna try again? ' )) {
+			$self->setup_name;
+		} else {
+			print "[ERROR] A name is required to work with DuckPAN\n";
+			exit 1;
+		}
+	}
+}
+
+sub setup_email {
+	my ( $self ) = @_;
+	my $email = $self->get_email;
+	if (Email::Valid->address($email)) {
+		$self->email($email);
+	} else {
+		print "No valid email given!\n";
+		if ($self->term->ask_yn( prompt => 'Wanna try again? ' )) {
+			$self->setup_email;
+		} else {
+			print "[ERROR] An email is required to work with DuckPAN\n";
+			exit 1;
+		}
+	}
 }
 
 sub setup_dukgo {
 	my ( $self ) = @_;
-	my $user = $self->get_user;
+	my $user = $self->has_user ? $self->user : $self->get_user;
 	my $pass = $self->get_pass;
-	print "Checking your account on https://dukgo.com/... ";
+	print "\nChecking your account on https://dukgo.com/... ";
 	if ($self->app->checking_dukgo_user($user,$pass)) {
 		print "success!\n";
 		$self->user($user);
@@ -62,6 +151,7 @@ sub setup_dukgo {
 	} else {
 		print "failed!\n";
 		if ($self->term->ask_yn( prompt => 'Wanna try again? ' )) {
+			$self->clear_user if $self->has_user;
 			$self->setup_dukgo;
 		} else {
 			print "[ERROR] A login to https://dukgo.com/ is required to work with DuckPAN\n";
--- lib/App/DuckPAN/Perl.pm
+++ lib/App/DuckPAN/Perl.pm
@@ -5,11 +5,37 @@ with 'App::DuckPAN::HasApp';
 
 use Config::INI;
 use Dist::Zilla::Util;
+use Path::Class;
+use Config::INI::Reader;
+use Data::Dumper;
+
+sub dzil_root { Dist::Zilla::Util->_global_config_root }
+sub dzil_config { file(shift->dzil_root,'config.ini') }
 
 sub setup {
-	my ( $self ) = @_;
+	my ( $self, %params ) = @_;
 	my $config_root = Dist::Zilla::Util->_global_config_root;
 	$config_root->mkpath unless -d $config_root;
+	my $config = $self->get_dzil_config;
+	$config = {} unless $config;
+	$config->{'%Rights'} = {
+		license_class => 'Perl_5',
+		copyright_holder => $params{name},
+	} unless defined $config->{'%Rights'};
+	$config->{'%User'} = {
+		email => $params{email},
+		name => $params{name},
+	} unless defined $config->{'%User'};
+	$config->{'%DUKGO'} = {
+		username => $params{dukgo_user},
+		password => $params{dukgo_pass},
+	};
+}
+
+sub get_dzil_config {
+	my ( $self ) = @_;
+	return unless -d $self->dzil_root && -f $self->dzil_config;
+	Config::INI::Reader->read_file($self->dzil_config);
 }
 
 1;
