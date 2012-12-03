70309612fd0223fc3a5a8f5d2cfe87e2dc5c1895
Michael Smith <crazedpsyc@duckduckgo.com>
2012-12-03 10:07:55 -0700

    Broaden deprecation message for 'duckpan goodie'

--- lib/App/DuckPAN/Cmd/Goodie.pm
+++ lib/App/DuckPAN/Cmd/Goodie.pm
@@ -10,18 +10,8 @@ use Data::Printer;
 
 sub run {
 	my ( $self, @args ) = @_;
-	if (defined $args[0]) {
-		unless ($self->app->get_local_ddg_version) {
-			print "\n[ERROR] You need to have the DDG distribution installed\n";
-			print "\nTo get the installation command, please run: duckpan check\n\n";
-			exit 1;
-		}
-		if ($args[0] eq 'test') {
-			print "\n[DEPRECATED] Please use \"duckpan query\"!\n";
-			exit 1;
-		}
-	} 
-	print $self->app->help->goodie;
+        print "\n[DEPRECATED] Please use \"duckpan query\"!\n";
+        exit 1;
 }
 
 1;
