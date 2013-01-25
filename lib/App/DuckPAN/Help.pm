82c344adb507b6f163ed7b00b6b403bae446f0e6
Dylan Lloyd <dylan@dylansserver.com>
2013-01-24 22:41:45 -0500

    reformatted help text

--- lib/App/DuckPAN/Help.pm
+++ lib/App/DuckPAN/Help.pm
@@ -6,45 +6,40 @@ has version => ( is => 'ro', required => 1 );
 
 sub header { my $version = shift->version; return <<"__EOT__"; }
 
-  ____    V$version   _    ____   _    _   _
- |  _ \\ _   _  ___| | _|  _ \\ / \\  | \\ | |
- | | | | | | |/ __| |/ / |_) / _ \\ |  \\| |
- | |_| | |_| | (__|   <|  __/ ___ \\| |\\  |
- |____/ \\__,_|\\___|_|\\_\\_| /_/   \\_\\_| \\_|
+ ____   V$version    _    ____   _    _   _
+|  _ \\ _   _  ___| | _|  _ \\ / \\  | \\ | |
+| | | | | | |/ __| |/ / |_) / _ \\ |  \\| |
+| |_| | |_| | (__|   <|  __/ ___ \\| |\\  |
+|____/ \\__,_|\\___|_|\\_\\_| /_/   \\_\\_| \\_|
 
-  Contributing to https://duckduckgo.com/
-  =======================================
+ Contributing to https://duckduckgo.com/
+ =======================================
 
 __EOT__
 
 sub help { return shift->header().<<'__EOT__'; }
+Usage:
 
-duckpan check
--------------
- Check if you fulfill all requirements for the development
- environment (this is run automatically during setup)
+- duckpan check
+    Check if you fulfill all requirements for the development
+    environment (this is run automatically during setup)
 
-duckpan installdeps
--------------
- Install all requirements of the specific DuckDuckHack project (if
- possible), like zeroclickinfo-spice, zeroclickinfo-goodie, duckduckgo
- or community-platform
+- duckpan installdeps
+    Install all requirements of the specific DuckDuckHack project (if
+    possible), like zeroclickinfo-spice, zeroclickinfo-goodie, duckduckgo
+    or community-platform
 
-duckpan query
--------------
- Test goodies and spice interactively on the command line
+- duckpan query
+    Test goodies and spice interactively on the command line
 
-duckpan server
---------------
- Test spice on a local web server
+- duckpan server
+    Test spice on a local web server
 
-duckpan test
-------------
- Test your release (this will run automatically before a release)
+- duckpan test
+    Test your release (this will run automatically before a release)
 
-duckpan release
----------------
- Release the project of the current directory to DuckPAN
+- duckpan release
+    Release the project of the current directory to DuckPAN
 
 __EOT__
 
