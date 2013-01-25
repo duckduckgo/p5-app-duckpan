4b80ada08928b2da120ca3683d7c7c88faae38ec
Dylan Lloyd <dylan@dylansserver.com>
2013-01-24 20:00:10 -0500

    list env variables & values with `duckpan env key`

--- lib/App/DuckPAN/Cmd/Env.pm
+++ lib/App/DuckPAN/Cmd/Env.pm
@@ -6,18 +6,28 @@ use MooX qw( Options );
 with qw( App::DuckPAN::Cmd );
 
 sub run {
-	my ( $self, $name, $key ) = @_;
+    my ( $self, $name, $key ) = @_;
+
+    my $config_file = file($self->app->cfg->cache_path, 'env.ini');
+
+    if (defined $name and $name eq 'key') {
+      my $config = Config::INI::Reader->read_file($config_file);
+      map { print "$_ = $config->{'_'}{$_}\n" } keys $config->{'_'};
+      exit 0;
+    }
 
     if (not defined $name or not defined $key) {
-      print STDERR "Usage: duckpan env <name> <value>";
+      printf STDERR "Usage:\n";
+      printf STDERR "  %10s\tduckpan env <name> <value>\n", "to add:";
+      printf STDERR "  %10s\tduckpan env key\n", "to view:";
       exit 1;
     }
 
     my $config = { '_' => { "$name" => "$key" } };
 
-    open my $file, '>>', file($self->app->cfg->cache_path, 'env.ini');
+    open my $output, '>>', $config_file;
 
-    Config::INI::Writer->write_handle($config, $file);
+    Config::INI::Writer->write_handle($config, $output);
 
     print "Added '$name = $key' to duckpan env\n";
 
