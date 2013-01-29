f3abe43ef25942e8a98d6a125d352398becf7748
Dylan Lloyd <dylan@dylansserver.com>
2013-01-29 16:27:30 -0500

    error w/o key to rm, & don't output on success

--- lib/App/DuckPAN/Cmd/Env.pm
+++ lib/App/DuckPAN/Cmd/Env.pm
@@ -27,7 +27,7 @@ sub run {
            Config::INI::Writer->write_handle($config, $output);
            exit 0;
          }
-         exit 0;
+         exit 0 if defined $key;
       } elsif ($config->{'_'} and grep {$_ eq $name} keys $config->{'_'}) {
         print STDERR "$name is already defined in env.ini\n";
         exit 1;
@@ -37,17 +37,14 @@ sub run {
     if (not defined $name or not defined $key) {
       printf STDERR "Usage:\n";
       printf STDERR "  %10s\tduckpan env <name> <value>\n", "to add:";
+      printf STDERR "  %10s\tduckpan rm <name>\n", "to remove:";
       printf STDERR "  %10s\tduckpan env key\n", "to view:";
       exit 1;
     }
 
     my $config = { '_' => { "$name" => "$key" } };
-
     open my $output, '>>', $config_file;
-
     Config::INI::Writer->write_handle($config, $output);
-
-    print "Added '$name = $key' to duckpan env\n";
 
     exit 0;
 
