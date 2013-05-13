eb704744767807cb45e4b53bf04c727af7cabc2b
Torsten Raudssus <torsten@raudss.us>
2013-05-13 02:30:09 +0200

    Added secret roadrunner feature

---
+++ lib/App/DuckPAN/Cmd/Roadrunner.pm
@@ -0,0 +1,26 @@
+package App::DuckPAN::Cmd::Roadrunner;
+
+use Moo;
+with qw( App::DuckPAN::Cmd );
+
+use MooX::Options;
+
+sub run {
+	my ( $self, @args ) = @_;
+
+	if (-f 'dist.ini') {
+		$self->app->print_text(
+			"Found a dist.ini, suggesting a Dist::Zilla distribution",
+		);
+		$self->app->perl->cpanminus_install_error
+			if (system("dzil authordeps --missing 2>/dev/null | grep -vP '[^\\w:]' | cpanm --quiet --notest --skip-satisfied"));
+		$self->app->perl->cpanminus_install_error
+			if (system("dzil listdeps --missing 2>/dev/null | grep -vP '[^\\w:]' | cpanm --quiet --notest --skip-satisfied"));
+		$self->app->print_text(
+			"Everything fine!",
+		);
+	}
+
+}
+
+1;
