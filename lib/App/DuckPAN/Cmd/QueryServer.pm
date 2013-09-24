de388b9bc6f4842daad39f18b25a9367521ac505
Supreet Sethi <supreet.sethi@gmail.com>
2013-09-24 18:22:14 +0530

    Refactoring the QueryServer Command

--- lib/App/DuckPAN/Cmd/QueryServer.pm
+++ lib/App/DuckPAN/Cmd/QueryServer.pm
@@ -10,7 +10,65 @@ use URI::Escape;
 use JSON;
 
 
-my $ua = LWP::UserAgent->new;
+sub test_goodie {
+    my $ex = shift @_;
+    my $answer_type = shift @_;
+    my $ua = LWP::UserAgent->new;
+    my $cooked_ex = uri_escape_utf8($ex);
+    my $url = 'https://api.duckduckgo.com?q='.$cooked_ex.'&o=json&no_html=1&d=0';
+    my $req = HTTP::Request->new(GET => $url);
+    my $res = $ua->request($req);
+    my $content_type = $res->headers->{'content-type'};
+    if ($res-> is_success && $content_type =~ /application\/x-javascript/ ){
+	my $test_res =  from_json($res->content);
+	if ($test_res->{"AnswerType"} =~ $answer_type)
+	{
+	    print '  '. $ex . " Test OK\n";
+	}
+	else
+	{
+	    print '  '. $ex . " Test FAIL\n";
+	}
+    }
+    else
+    {
+	if (!$content_type =~ /application\/x-javascript/)
+	{
+	    print "  ". $ex . "Content does not match query\n";
+	}
+	else 
+	{	    
+	print "  ". $ex . "Network Error\n";
+	}
+    }
+}
+
+
+sub test_spice {
+    my $ex = shift @_;
+    my $kickstart = shift @_;
+    my $ua = LWP::UserAgent->new;
+    my $cooked_ex = uri_escape_utf8($ex);
+    my $url = 'https://duckduckgo.com?q='.$cooked_ex;
+    my $req = HTTP::Request->new(GET => $url);
+    my $res = $ua->request($req);
+    if ($res-> is_success){
+	
+	if ($res->content =~ /\Q$kickstart\E/)
+	{
+	    print '  '. $ex . " Test OK\n";
+	}
+	else
+	{
+	    print '  '. $ex . " Test FAIL\n";
+	}
+    }
+    else
+    {
+	print "  ". $ex . "Network Error\n";
+    }
+}
+
 
 sub test_query {
     my $test_subject = shift @_;
@@ -19,69 +77,31 @@ sub test_query {
     print "\n".$plugin_name."\n";
     my $examples_ref = $test_subject->{'examples'};
     my @examples = @{$examples_ref};
-    	
-    if (exists $test_subject->{'answer_type'})
+    foreach my $ex (@examples)
     {
-	my $answer_type = $test_subject->{'answer_type'};
-	foreach my $ex (@examples)
+	if (exists $test_subject->{'answer_type'})
 	{
-	    my $cooked_ex = uri_escape_utf8($ex);
-	    my $url = 'https://api.duckduckgo.com?q='.$cooked_ex.'&o=json&no_html=1&d=0';
-	    my $req = HTTP::Request->new(GET => $url);
-	    my $res = $ua->request($req);
-	    if ($res-> is_success){
-		my $test_res =  from_json($res->content);
-		if ($test_res->{"AnswerType"} =~ $answer_type)
-		{
-		    print '  '. $ex . " Test OK\n";
-		}
-		else
-		{
-		    print '  '. $ex . " Test FAIL\n";
-		}
-	       }
-	    else
+	    my $answer_type = $test_subject->{'answer_type'};
+	    test_goodie($ex, $answer_type);
+	}
+	else
+	{
+	    if ($plugin_name =~ /Goodie/)
 	    {
-		print "  ". $ex . "Network Error\n";
+		# Situation where answer_type is not defined for Goodie
+		return;
+	    }
+
+	    if (exists $test_subject->{'kickstart'})
+	    {
+		my $kickstart = $test_subject->{'kickstart'};
+		test_spice($ex, $kickstart);
+
 	    }
 	}
-	
     }
-    else
-    {
-	if ($plugin_name =~ /Goodie/)
-	{
-	    return;
-	}
-	if (exists $test_subject->{'kickstart'})
-	{
-	    my $kickstart = $test_subject->{'kickstart'};
-	    foreach my $ex (@examples)
-	    {
-		my $cooked_ex = uri_escape_utf8($ex);
-		my $url = 'https://duckduckgo.com?q='.$cooked_ex;
-		my $req = HTTP::Request->new(GET => $url);
-		my $res = $ua->request($req);
-		if ($res-> is_success){
-		    
-		    if ($res->content =~ /\Q$kickstart\E/)
-		    {
-			print '  '. $ex . " Test OK\n";
-		    }
-		    else
-		    {
-			print '  '. $ex . " Test FAIL\n";
-		    }
-		}
-		else
-		{
-		    print "  ". $ex . "Network Error\n";
-		}
-	    }
-	}
-	    
-    }
-}   
+}
+	   
 
 
 
