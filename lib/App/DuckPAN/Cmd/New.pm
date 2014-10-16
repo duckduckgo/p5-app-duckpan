c3ce8c91461a92601a501512d6742d34e6d6b40e
Zaahir Moolla <moollaza@gmail.com>
2014-10-15 21:55:12 -0400

    improve handling of names to support filepaths or package names, cleanup code

--- lib/App/DuckPAN/Cmd/New.pm
+++ lib/App/DuckPAN/Cmd/New.pm
@@ -11,10 +11,10 @@ package App::DuckPAN::Cmd::New;
 # 	- <name.handlebars> is created in /share/spice/<name>
 
 use Moo;
-use Data::Dumper;
 with qw( App::DuckPAN::Cmd );
 use Text::Xslate qw(mark_raw);
 use IO::All;
+use File::Path qw(make_path);
 
 sub run {
 	my ( $self, @args ) = @_;
@@ -22,95 +22,99 @@ sub run {
 	# Instant Answer name as parameter
 	my $entered_name = (@args) ? join(' ', @args) : $self->app->get_reply('Please enter a name for your Instant Answer');
 	my $name = $self->app->phrase_to_camel($entered_name);
+	my ($path, $lc_path) = ("", "");
+	my $package_name = $name;
+
+	if ($entered_name =~ m/::|\//) {
+		my @path_parts = split(/::|\//, $entered_name);
+		$name = pop @path_parts;
+		$path = join("/", @path_parts);
+		$path .= "/";
+		$lc_path = $self->app->camel_to_underscore($path);
+		$package_name =~ s/\//::/;
+	}
+
 	my $lc_name = $self->app->camel_to_underscore($name);
 
 	# %templates forms the spine data structure which is used
 	# as a guide to discovering content which is moved around
 	my %templates = (
-		goodie => {
+		Goodie => {
 			lib_dir => "./lib/DDG/Goodie",
+			dirs => [
+				"./lib/DDG/Goodie/$path",
+				"./t/$path",
+			],
 			files => {
-				"./template/lib/DDG/Goodie/Example.pm" => "./lib/DDG/Goodie/$name.pm",
-				"./template/t/Example.t" => "./t/$name.t",
+				"./template/lib/DDG/Goodie/Example.pm" => "./lib/DDG/Goodie/$path"."$name.pm",
+				"./template/t/Example.t" => "./t/$path"."$name.t",
 			},
 		},
 
-		spice => {
+		Spice => {
 			lib_dir => "./lib/DDG/Spice",
-			share_dir => "./share/spice/$lc_name",
+			share_dir => "./share/spice/$lc_path"."$lc_name",
+			dirs => [
+				"./lib/DDG/Spice/$path",
+				"./t/$path",
+				"./share/spice/$lc_path",
+				"./share/spice/$lc_path",
+			],
 			files => {
-				"./template/lib/DDG/Spice/Example.pm" => "./lib/DDG/Spice/$name.pm",
-				"./template/t/Example.t" => "./t/$name.t",
-				"./template/share/spice/example/example.handlebars" => "./share/spice/$lc_name/$lc_name.handlebars",
-				"./template/share/spice/example/example.js" => "./share/spice/$lc_name/$lc_name.js"
+				"./template/lib/DDG/Spice/Example.pm" => "./lib/DDG/Spice/$path"."$name.pm",
+				"./template/t/Example.t" => "./t/$path"."$name.t",
+				"./template/share/spice/example/example.handlebars" => "./share/spice/$lc_path"."$lc_name/$lc_name.handlebars",
+				"./template/share/spice/example/example.js" => "./share/spice/$lc_path"."$lc_name/$lc_name.js"
 			}
+		},
+		Fathead => {
+			lib_dir => "./lib/DDG/Fathead",
+			# [TODO] Implement Fathead templates
+		},
+		Longtail => {
+			lib_dir => "./lib/DDG/Longtail",
+			# [TODO] Implement Fathead templates
 		}
 	);
 
-	# Check if we're in a Goodie repository
-	if (-d $templates{'goodie'}{'lib_dir'}) {
-		
-		for my $filename (keys %{$templates{'goodie'}{'files'}}) {
-			my $dest = $templates{'goodie'}{'files'}{$filename};
-			if (-e $dest) {
-				$self->app->print_text("[ERROR] File already exists: $name $dest");
-				exit -1;
-			}
-			unless (-e $filename) {
-				$self->app->print_text("[ERROR] Template does not exist: $filename");
-				exit -1;
-			}
-		}
-
-		while (my ($source, $dest) = each(%{$templates{'goodie'}{'files'}})) {
-			my $tx = Text::Xslate->new();
-			my %vars = (ia_name => $name,	lia_name => $lc_name);
-			my $content = $tx->render($source, \%vars);
-			io($dest)->append($content);
-			$self->app->print_text("Created file: $dest");
-		}
-		$self->app->print_text("Successfully created Goodie: $name ");
-	}
-
-	# Check if we're in a Spice repository
-	elsif (-d $templates{'spice'}{'lib_dir'}) {
-
-		if (-d $templates{'spice'}{'share_dir'}) {
-			$self->app->print_text("[ERROR] Instant answer already exists: $name");
-			exit -1;
-		}	else {
-			for my $filename (keys %{$templates{'spice'}{'files'}}) {
-				my $dest = $templates{'spice'}{'files'}{$filename};
-				if (-e $dest) {
-					$self->app->print_text("[ERROR] File already exists: $name $dest");
-					exit -1;
-				}
-				unless (-e $filename) {
-					$self->app->print_text("[ERROR] Template does not exist: $filename");
-					exit -1;
-				}
-			}
-
-			mkdir $templates{'spice'}{'share_dir'};
-			while (my ($source, $dest) = each(%{$templates{'spice'}{'files'}})) {
-				my $tx = Text::Xslate->new();
-				my %vars = (ia_name => $name,
-					lia_name => $lc_name);
-				my $content = $tx->render($source, \%vars);
-
-				io($dest)->append($content);
-
-				$self->app->print_text("Created file: $dest");
-			}
-			$self->app->print_text("Successfully created Spice: $name");
-		}
-	}
-
-	# [TODO] Implement Fathead and Longtail templates
-
-	else {
+	# Check which IA repo we're in...
+	my $type = "";
+	if (-d $templates{'Goodie'}{'lib_dir'}) {
+		$type = "Goodie";
+	} elsif (-d $templates{'Spice'}{'lib_dir'}) {
+		$type = "Spice";
+	} elsif (-d $templates{'Fathead'}{'lib_dir'}) {
+		$type = "Fathead";
+		$self->app->print_text("[ERROR] Sorry, DuckPAN does not support Fatheads yet!");
+		exit -1;
+	} elsif (-d $templates{'Longtail'}{'lib_dir'}) {
+		$type = "Longtail";
+		$self->app->print_text("[ERROR] Sorry, DuckPAN does not support Longtails yet!");
+		exit -1;
+	} else {
 		$self->app->print_text("[ERROR] No lib/DDG/Goodie, lib/DDG/Spice, lib/DDG/Fathead or lib/DDG/Longtail found");
 	}
+
+	while (my ($source, $dest) = each(%{$templates{$type}{'files'}})) {
+		my $io = io($dest);
+		if ($io->exists) {
+			my $filename = $io->filename;
+			my $filepath= $io->filepath;
+			$self->app->print_text("[ERROR] File already exists: \"$filename\" in $filepath");
+			exit -1;
+		}
+		unless (-e $source) {
+			$self->app->print_text("[ERROR] Template does not exist: $source");
+			exit -1;
+		}
+
+		my $tx = Text::Xslate->new();
+		my %vars = (ia_name => $name, lia_name => $lc_name);
+		my $content = $tx->render($source, \%vars);
+		$io->file->assert->append($content); #create file path and append to file
+		$self->app->print_text("Created file: $dest");
+	}
+	$self->app->print_text("Successfully created $type: $name");
 }
 
 1;
