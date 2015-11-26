6ec078528bb38606856d1b498e32f2579a803910
Sarvesh D <sarveshd@openmailbox.org>
2015-11-26 18:34:03 +0530

    Restart.pm: Use template info for monitoring
    of directories which need the server to be restarted

--- lib/App/DuckPAN/Restart.pm
+++ lib/App/DuckPAN/Restart.pm
@@ -4,6 +4,8 @@ package App::DuckPAN::Restart;
 use File::Find::Rule;
 use Filesys::Notify::Simple;
 
+use App::DuckPAN::TemplateDefinitions;
+
 use strict;
 
 use Moo::Role;
@@ -66,11 +68,15 @@ sub _monitor_directories {
     # Note: Could potentially be functionality added to App::DuckPAN
     # which would return the directories involved in an IA
     # (see https://github.com/duckduckgo/p5-app-duckpan/issues/200)
+    my $template_defs = App::DuckPAN::TemplateDefinitions->new;
+    my @templates = $template_defs->get_templates;
     my %distinct_dirs;
-    while(my ($type, $io) = each %{$self->app->get_ia_type()->{templates}}){
-        next if $type eq 'test'; # skip the test dir?
+
+    for my $template (@templates) {
+	next unless $template->needs_restart;
+
         # Get any subdirectories
-        my @d = File::Find::Rule->directory()->in($io->{out});
+        my @d = File::Find::Rule->directory()->in($template->output_directory);
         # We don't know what templates will contain, e.g. subdiretories
         ++$distinct_dirs{$_} for @d;
     }
