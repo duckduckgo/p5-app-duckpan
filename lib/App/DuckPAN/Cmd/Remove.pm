8ae997329b1f9612a9cc5cf5b2937f20b0947dc6
Ben Moon <guiltydolphin@gmail.com>
2016-05-05 16:21:41 +0100

    Add 'remove' command
    Command for removing Instant Answers and Instant Answer files.

---
+++ lib/App/DuckPAN/Cmd/Remove.pm
@@ -0,0 +1,30 @@
+package App::DuckPAN::Cmd::Remove;
+# ABSTRACT: Command for removing Instant Answers and Instant
+# Answer files.
+
+use Moo;
+use App::DuckPAN::InstantAnswer::Config;
+use MooX::Options;
+
+with qw( App::DuckPAN::Cmd App::DuckPAN::InstantAnswer::Cmd );
+
+sub run {
+	my $self = shift;
+	my $ia = $self->_ask_ia_check();
+	my $ia_cfg = App::DuckPAN::InstantAnswer::Config->new(ia => $ia);
+	my %files = %{$ia_cfg->files()};
+	my @files = @{$files{all}};
+	$self->app->emit_info(
+		"This will remove the following files:\n" .
+		join "\n", @files
+	);
+	if ($self->app->ask_yn('Continue?')) {
+		foreach my $file (@files) {
+			$file->remove();
+		}
+	}
+}
+
+1;
+
+__END__
