a7689803f8a85cefefd20157d749eacbcd49f842
Ben Moon <guiltydolphin@gmail.com>
2016-05-05 16:31:50 +0100

    Add handler config to Perl back-end templates

--- lib/App/DuckPAN/Template/Definitions.pm
+++ lib/App/DuckPAN/Template/Definitions.pm
@@ -41,6 +41,51 @@ sub _get_config_cheat_sheet {
 	};
 }
 
+# Allow user to choose a handler
+sub _get_config_handler {
+	my %options = @_;
+	my $app = $options{app};
+
+	my @handlers = (
+		# Scalar-based
+		'remainder: (default) The query without the trigger words, spacing and case are preserved.',
+		'query_raw: Like remainder but with trigger words intact',
+		'query: Full query normalized with a single space between terms',
+		'query_lc: Like query but in lowercase',
+		'query_clean: Like query_lc but with non-alphanumeric characters removed',
+		'query_nowhitespace: All whitespace removed',
+		'query_nowhitespace_nodash: All whitespace and hyphens removed',
+		# Array-based
+		'matches: Returns an array of captured expression from a regular expression trigger',
+		'words: Like query_clean but returns an array of the terms split on whitespace',
+		'query_parts: Like query but returns an array of the terms split on whitespace',
+		'query_raw_parts: Like query_parts but array contains original whitespace elements'
+	);
+
+	my $res = $app->get_reply(
+		'Which handler would you like to use to process the query?',
+		choices => \@handlers,
+		default => $handlers[0]
+	);
+
+	unless($res =~ /^([^:]+)/){
+		$app->emit_and_exit(-1, "Failed to extract handler from response: $res");
+	}
+	my $handler = $1;
+	my %check = map { $_ => 1 } ('words', 'query_parts', 'query_raw_parts', 'matches');
+	my $var = $check{$handler} ? '@' : '$';
+	my $trigger = $handler eq 'matches'
+		? q{query => qr/trigger regex/}
+		: q{any => 'triggerword', 'trigger phrase'};
+
+	return {
+		ia_handler     => $handler,
+		ia_handler_var => $var,
+		ia_trigger     => $trigger
+	};
+}
+
+
 my %templates = (
 	cheat_sheet => {
 		label       => 'Cheat Sheet',
@@ -60,12 +105,14 @@ my %templates = (
 	pm => {
 		label       => 'Perl Module',
 		allow       => [\&is_full_goodie, \&is_spice],
+		configure   => \&_get_config_handler,
 		input_file  => '<:$repo.template_dir:>/perl_main.tx',
 		output_file => 'lib/<:$package_separated:>.pm',
 	},
 	min_pm => {
 		label       => 'Minimal Perl Module',
 		allow       => \&is_full_goodie,
+		configure   => \&_get_config_handler,
 		input_file  => '<:$repo.template_dir:>/perl_main_basic.tx',
 		output_file => 'lib/<:$package_separated:>.pm',
 	},
