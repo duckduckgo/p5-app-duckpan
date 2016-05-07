310dc2c595a06a1b8056c67e8834dff172623aa5
Ben Moon <guiltydolphin@gmail.com>
2016-05-07 14:14:34 +0100

    Update TemplateSets to represent template sets
    Instead of having optional/required templates, a TemplateSet *just* (for
    now) represents a set of templates.
    
    This simplifies things as we can allow the user to further configure
    templates through DuckPAN/other commands; 'new' just generates the base
    boilerplate.

---
+++ lib/App/DuckPAN/Template/Set.pm
@@ -0,0 +1,62 @@
+package App::DuckPAN::Template::Set;
+# ABSTRACT: TemplateSet definitions
+
+use App::DuckPAN::TemplateSet;
+use App::DuckPAN::Template::Definitions;
+use Moo;
+with qw( App::DuckPAN::Lookup );
+
+my $template_defs = App::DuckPAN::Template::Definitions->new();
+
+my %default_template_sets = (
+	goodie_standard => {
+		description => 'Standard Goodie Instant Answer',
+		templates    => [
+			$template_defs->get_templates(
+				id => 'pm', id => 'test',
+			),
+		],
+	},
+	cheat_sheet => {
+		description => 'Cheat Sheet Instant Answer',
+		templates   => [
+			$template_defs->get_templates(id => 'cheat_sheet'),
+		],
+	},
+);
+
+has definitions => (
+	is      => 'ro',
+	default => sub { \%default_template_sets },
+);
+
+has template_sets => (
+	is      => 'ro',
+	builder => 1,
+);
+
+sub _lookup {
+	my $self = shift;
+	return $self->template_sets;
+}
+
+sub _lookup_id {
+	return qw( id );
+}
+
+sub _build_template_sets {
+	my $self = shift;
+	my @template_sets;
+	while (my ($template, $definition) = each %{$self->definitions}) {
+		my $template_set = App::DuckPAN::TemplateSet->new(
+			id => $template,
+			%$definition,
+		);
+		push @template_sets, $template_set;
+	}
+	return \@template_sets;
+}
+
+1;
+
+__END__
--- lib/App/DuckPAN/TemplateSet.pm
+++ lib/App/DuckPAN/TemplateSet.pm
@@ -4,18 +4,9 @@ package App::DuckPAN::TemplateSet;
 # A group of templates is a template set. Conceptually this represents a
 # sub-type of an instant answer type. For example, a Goodie can be a standard
 # Goodie or a Cheat Sheet goodie, each of which corresponds to a template set.
-#
-# Each template set can have required and optional templates. 'required' templates
-# are always used to generate output files, while the user's confirmation is
-# needed before each optional template is processed.
 
 use Moo;
-
-use Try::Tiny;
 use List::Util qw(all);
-use Algorithm::Combinatorics qw(combinations);
-use Carp;
-
 use namespace::clean;
 
 has id => (
@@ -30,110 +21,24 @@ has description => (
 	doc      => 'Description of the template set',
 );
 
-has subdir_support => (
-	is       => 'ro',
-	default  => 1,
-	doc      => 'Does this template set support creation of Instant Answers inside sub-directories? ' .
-				'For example, Cheat Sheet Instant Answers do not support it, while Standard Goodie ' .
-				'and Spice ones do have support.',
-);
-
-has required_templates => (
+has templates => (
 	is       => 'ro',
 	required => 1,
-	doc      => 'Arrayref of App::DuckPAN::Template instances that represent mandatory templates',
+	doc      => 'Templates that comprise the template set.',
 );
 
-has optional_templates => (
-	is       => 'ro',
-	required => 1,
-	doc      => 'Arrayref of App::DuckPAN::Template instances that represent optional templates',
-);
-
-has optional_template_combinations => (
-	is       => 'ro',
-	lazy     => 1,
-	builder  => 1,
-	init_arg => undef,
-	doc      => 'Arrayref of possible optional template combinations, ' .
-				'which themselves are arrayrefs of templates.',
-);
-
-# All possible template combinations are generated from the list in the
-# 'optional_templates' attribute. They are sorted by the following rules:
-#
-#   1. Combinations are sorted by length (ascending)
-#   2. In each combination, the templates are sorted in the same order that
-#      they appear in the 'optional_templates' attribute
-#   3. Combinations of the same length are then sorted based on the same rule
-sub _build_optional_template_combinations {
-	my $self = shift;
-	my @templates = @{$self->optional_templates};
-
-	# Map of template -> position in template list
-	my %template_pos = map { ($templates[$_] => $_) } 0..$#templates;
-
-	# All combinations of all lengths; sorted
-	my @template_combinations;
-
-	for my $length (1..@templates) {
-		# Generate all combinations of length $length
-		my @combinations = combinations(\@templates, $length);
-
-		# Sort the tempates in each combination
-		for my $combination (@combinations) {
-			@$combination = sort { $template_pos{$a} <=> $template_pos{$b} } @$combination;
-		}
-
-		# Sort the array of combinations
-		@combinations = sort {
-			# The comparison function compares two arrayrefs of templates.
-			# Templates from both the arrayrefs are compared one by one and the
-			# function returns the value when it finds a difference.
-			for my $i (0..$length-1) {
-				my $cmp = $template_pos{$a->[$i]} <=> $template_pos{$b->[$i]};
-
-				return $cmp;
-			}
-		} @combinations;
-
-		push @template_combinations, @combinations;
-	}
-
-	return \@template_combinations;
+# A TemplateSet supports an Instant Answer if all of its templates
+# do.
+sub supports {
+	my ($self, $ia) = @_;
+	return all { $_->supports($ia) } @{$self->templates};
 }
 
-# check if all templates in the array @templates are optional
-sub _are_templates_optional {
-	my ($self, @templates) = @_;
-	my %optional_templates = map { ($_ => 1) } @{$self->optional_templates};
-
-	return all { $optional_templates{$_} } @templates;
-}
-
-# Use the template to generate necessary files. Takes 2 parameters:
-# $vars: variables to substitute in the templates
-# $optional_templates: arrayref of template instances taken from $self->optional_templates
 sub generate {
-	my ($self, $vars, $optional_templates) = @_;
-	my @created_files;
-	my $error;
-
-	# Verify that $optional_templates has templates from within $self->optional_templates
-	$self->_are_templates_optional(@$optional_templates)
-		or croak("Unknown template(s) in \$optional_templates");
-
-	for my $template (@{$self->required_templates}, @$optional_templates) {
-		try {
-			push @created_files, $template->generate($vars);
-		} catch {
-			$error = $_;
-		};
-
-		last if $error;
+	my ($self, %options) = @_;
+	foreach my $template (@{$self->templates}) {
+		$template->configure(%options);
 	}
-
-	return (created_files => [ @created_files ], error => $error);
 }
 
 1;
