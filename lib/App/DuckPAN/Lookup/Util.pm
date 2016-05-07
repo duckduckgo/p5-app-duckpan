57d23873304b6aa9b4ea02218e6924fc4d5c01a2
Ben Moon <guiltydolphin@gmail.com>
2016-05-07 17:32:48 +0100

    Move lookup operations to Lookup::Util
    So we can access the lookup sub without needing to use the Role.

---
+++ lib/App/DuckPAN/Lookup/Util.pm
@@ -0,0 +1,39 @@
+package App::DuckPAN::Lookup::Util;
+# ABSTRACT: Utilities for standardized lookups.
+
+BEGIN {
+	require Exporter;
+
+	our @ISA = qw(Exporter);
+
+	our @EXPORT_OK = qw(lookup);
+}
+
+use List::Util qw(pairs);
+
+sub satisfy {
+	my ($parent, $by, $lookup) = @_;
+	if (ref $by eq 'CODE') {
+		return $by->($parent, $lookup);
+	}
+	my $pby = ref $parent eq 'HASH'
+		? $parent->{$by} : $parent->$by;
+	ref $pby eq 'CODE' ? $pby->($lookup) : $pby eq $lookup;
+}
+
+sub lookup {
+	my ($lookup_vals, $lookup_id, @lookups) = @_;
+	my @items = @$lookup_vals;
+	return @items unless @lookups;
+	my %results;
+	foreach (pairs @lookups) {
+		my ($by, $lookup) = @$_;
+		map { $results{$_->{$lookup_id}} = $_ }
+			grep { satisfy($_, $by, $lookup) } @items;
+	}
+	return values %results;
+}
+
+1;
+
+__END__
