a8dcefaddd0f74fc73092271c7c849d3f875b41e
Zaahir Moolla <moollaza@duckduckgo.com>
2016-07-20 15:14:55 -0400

    Switch back to Data::Printer for error message

--- lib/App/DuckPAN/Web.pm
+++ lib/App/DuckPAN/Web.pm
@@ -556,7 +556,7 @@ sub _no_results_error {
 			qq(<script type="text/javascript">seterr('$error')</script>)
 		)->guts
 	);
-	$self->app->emit_error("$error");
+	p($error, color => { string => 'red' });
 
 	my $body = $root->as_HTML;
 	$response->body($body);
